88cb1e6d802974304e4d54f894aee4cafa4775c8
scarerkite <scknight101@gmail.com>
2014-06-19 15:51:49 +0100

    Initial commit with files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootballHackathon::Application.config.secret_token = 'ee904cf51319340893c45b80f8f83b472fac49deb7d67bc7d344059d61dd8205b967520cf37da4525aca7d6f0ad7fbf0b4a45fa0f92106045d89831a2fd0a9cc'
